42fb36eeb15b5be71033ad073dbf178a9fac6808
Édes Balázs <bali182@users.noreply.github.com>
2015-05-14 09:54:49 +0200

    Added more functionality

--- Streams.ts
+++ Streams.ts
@@ -3,27 +3,51 @@
 /// <reference path="Iterable" />
 
 class Streams {
-	public static ofIterator<T>(iterator: Iterator<T>) {
+	public static ofIterator<T>(iterator: Iterator<T>): Stream<T> {
 		return new IteratorStream(iterator);
 	}
-	
-	public static ofArray<T>(array: Array<T>) {
+
+	public static ofArray<T>(array: Array<T>): Stream<T> {
 		return Streams.ofIterator(new ArrayIterator(array));
 	}
 
-	public static ofValues<T>(...values: Array<T>) {
+	public static ofValue<T>(value: T): Stream<T> {
+		return Streams.ofArray([value]);
+	}
+
+	public static ofValues<T>(...values: Array<T>): Stream<T> {
 		return Streams.ofArray(values);
 	}
 
-	public static ofIterable<T>(iterable: Iterable<T>) {
+	public static ofIterable<T>(iterable: Iterable<T>): Stream<T> {
 		return Streams.ofIterator(iterable.iterator());
 	}
 
-	public static generate<T>(supplier: () => T) {
+	public static generate<T>(supplier: () => T): Stream<T> {
 		return Streams.ofIterator(new EndlessIterator(supplier));
 	}
 
-	public static range(from: number, to: number, delta?: number) {
+	public static range(from: number, to: number, delta?: number): Stream<number> {
 		return Streams.ofIterator(new RangeIterator(from, to, delta));
+	}
+
+	public static concatenate<T>(...streams: Array<Stream<T>>): Stream<T> {
+		if (streams.length == 0) {
+			return Streams.empty<T>();
+		} else if (streams.length == 1) {
+			return streams[0];
+		} else {
+			return Streams.ofIterator(
+				Iterators.concatenate(
+					Iterators.map(
+						new ArrayIterator(streams), s => s.iterator()
+					)
+				)
+			);
+		}
+	}
+
+	public static empty<T>(): Stream<T> {
+		return Streams.ofIterator(Iterators.empty<T>());
 	}
 }
