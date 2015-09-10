914b5d49cba8b70c13a02ac8665ae24a84feb1ad
Édes Balázs <edesbalazs@gmail.com>
2015-09-10 08:02:29 +0200

    Added tslint. will add more rules, and fix the source.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
 	uglifiy = require('gulp-uglify'),
 	jasmine = require('gulp-jasmine'),
 	clean = require('gulp-clean'),
-	ts = require('gulp-typescript');
+	ts = require('gulp-typescript'),
+	tsLint = require('gulp-tslint');
 
 var REFERENCE_REPLACE_REGEX = /^\s*\/\/\/\s*<\s*reference\s*path\s*=\s*".*"\s*\/>\s*/mg;
 
@@ -22,51 +23,86 @@ var JS_MIN_SRC_FILE = 'tsf-min.js'
 var JS_SRC_FILE = 'tsf.js'
 var JS_TESTS_FILE = 'tsf-tests.js'
 
-var tsSourceCompiler = ts({
+var tsLintConfig = {
+	configuration: {
+		rules: {
+			'class-name': true,
+			/* 'curly': true,
+			'eofline': true,
+			'indent': [true, 'tabs'],
+			'max-line-length': 120,
+			'no-arg': true,
+			'no-conditional-assignment': true,
+			'no-console': [true,
+        'log',
+				'debug',
+        'info',
+        'time',
+        'timeEnd',
+        'trace'
+			],
+			'no-consecutive-blank-lines': true,
+			'no-construct': true,
+			'no-debugger': true,
+			'no-duplicate-key': true,
+			'no-duplicate-variable': true */
+		}
+	}
+};
+
+var tsSourceCompilerConfig = {
 	noImplicitAny: true,
 	declaration: true,
 	sortOutput: true,
 	out: JS_SRC_FILE,
 	outFile: JS_SRC_FILE
-});
+};
 
-var tsTestCompiler = ts({
+var tsTestCompilerConfig = {
 	removeComments: true,
 	sortOutput: true,
 	out: JS_TESTS_FILE,
 	outFile: JS_TESTS_FILE
+};
+
+gulp.task('lint', function () {
+	return gulp.src(SRC_FILES_EXPR)
+		.pipe(tsLint(tsLintConfig))
+		.pipe(tsLint.report('verbose', {
+			emitError: true,
+		}));
 });
 
 gulp.task('compile-src', function () {
 	var result = gulp.src(SRC_FILES_EXPR)
-		.pipe(tsSourceCompiler);
+		.pipe(ts(tsSourceCompilerConfig));
 	return merge([
 		result.js.pipe(replace(REFERENCE_REPLACE_REGEX, '')).pipe(gulp.dest(DIST_FOLDER)),
 		result.dts.pipe(replace(REFERENCE_REPLACE_REGEX, '')).pipe(gulp.dest(DIST_FOLDER)),
 	]);
 });
 
-gulp.task('test', function () {
+gulp.task('test', ['lint'], function () {
 	return gulp.src([SRC_FILES_EXPR, TEST_JASMINE, TEST_FILES_EXPR])
-		.pipe(tsTestCompiler).js
+		.pipe(ts(tsTestCompilerConfig)).js
 		.pipe(gulp.dest(DIST_FOLDER))
 		.pipe(jasmine({ verbose: true, includeStackTrace: true }));
-})
+});
 
 gulp.task('clean', function () {
 	return gulp.src([DIST_FOLDER])
 		.pipe(clean());
-})
+});
 
 gulp.task('minify', ['compile-src'], function () {
 	return gulp.src(DIST_FOLDER + '/' + JS_SRC_FILE)
 		.pipe(concat(JS_MIN_SRC_FILE)) // to have the desired filename
 		.pipe(uglifiy())
 		.pipe(gulp.dest(DIST_FOLDER));
-})
+});
 
 gulp.task('default', ['minify']);
 
-gulp.on('err', function(e) {
+gulp.on('err', function (e) {
 	console.log(e.err.stack)
-})
+});
